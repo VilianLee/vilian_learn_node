32947c0e4f1fe73ed64a6092bd430451b1c66bf9
VilianLee <276550763@qq.com>
2018-03-07 23:55:26 -0800

    1234

--- lesson01/show.js
+++ lesson01/show.js
@@ -1,11 +1,12 @@
 var fs = require("fs");
 
-console.log("查看 /tmp 目录");
-fs.readdir("/tmp/",function(err, files){
-   if (err) {
-          return console.error(err);
-	     }
-	        files.forEach( function (file){
-		       console.log( file );
-		          });
-			  });
+console.log("查看 当前 目录");
+fs.readdir(process.cwd(),function(error, files){
+   if (error) {
+   	return console.error(error);
+   }
+   files.forEach(
+   	function (file){
+   		console.log( file );
+   	});
+});
